6d34a6a9686210a4674b54fbffc960bf04100d3e
Indra Gunawan <feedsbrain@gmail.com>
2020-10-30 12:20:27 +0800

    Add _app.tsx

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const themeVariables = lessToJS(
 )
 
 module.exports = withLess({
+  cssModules: true,
   lessLoaderOptions: {
     javascriptEnabled: true,
     modifyVars: themeVariables, // make your antd custom effective
